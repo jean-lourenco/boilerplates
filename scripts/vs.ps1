c1031316092157426673e1bfcd493b81a647a91c
Jean Lourenço <jeancarlos.bnu@gmail.com>
2022-11-20 10:49:15 -0300

    Adicionar script vs.ps1
    Esse script é chamado em uma linha de comando Powershell simplesmente
    digitando `vs`, ao estilo de `code .`.
    O script procura por um arquivo `.sln` no diretório atual e abre-o no
    Visual Studio.
    Atualmente apenas o primeiro `.sln` é aberto, e se não houver nenhum sln
    o script vai quebrar.

---
+++ scripts/vs.ps1
@@ -0,0 +1,12 @@
+# Script vs.ps1
+# Utilização: `$ vs`
+# Função: Busca o primeiro arquivo .sln do repositório atual e abre-o no Visual Studio
+#         A variável $vs_path deve ser alterada para o diretório da sua instalação do VS
+#         Ou opcionalmente ela pode ser omitida se o devenv.exe estiver no PATH do sistema
+
+$vs_path = "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.exe"
+$sln_name = Get-ChildItem -Path . -Filter *.sln `
+| Select-Object -First 1 `
+| ForEach-Object { "$($_.BaseName)$($_.Extension)" }
+
+Start-Process $vs_path $sln_name
